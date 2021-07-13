c61b3f1efb127ae010690f26f8eba7666e51d03e
Ridha-gif <79398789+Ridha-gif@users.noreply.github.com>
2021-07-13 14:52:18 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDlTA56_XhGzzP2QaNXPBmoh7jEfsFnwdg",
+    authDomain: "class-work-7931b.firebaseapp.com",
+    projectId: "class-work-7931b",
+    storageBucket: "class-work-7931b.appspot.com",
+    messagingSenderId: "1001271260891",
+    appId: "1:1001271260891:web:6fc11e68c2c5c681a83efe",
+    measurementId: "G-YLTV8GWCVV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
